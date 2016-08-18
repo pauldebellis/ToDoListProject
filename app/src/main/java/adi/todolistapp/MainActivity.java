8600a9ceaf1cfb9df4f11a8dfa870df52d5437c9
pauldebellis <pvdebellis@gmail.com>
2016-08-17 17:08:56 -0700

    Made clicking a list item move to the next activity

--- app/src/main/java/adi/todolistapp/MainActivity.java
+++ app/src/main/java/adi/todolistapp/MainActivity.java
@@ -1,8 +1,10 @@
 package adi.todolistapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,12 +15,14 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private ArrayList<String> encounterList;
+    private ArrayList<String> combatantList;
     private ArrayAdapter<String> encountersArrayAdapter;
+    private ArrayAdapter<String> combatantsArrayAdapter;
     private ListView listView;
+    private ListView listView2;
     private EditText enterEncounter;
     private String newEncounter;
     private Button encounterButton;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,26 +30,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         listView = (ListView) findViewById(R.id.encounters);
+//        listView2 = (ListView) findViewById(R.id.combatantView);
 
         encounterList = new ArrayList<>();
+        combatantList = new ArrayList<>();
 
         encounterList.add("poop");
 
         encountersArrayAdapter = new ArrayAdapter<> (this, android.R.layout.simple_list_item_1, encounterList);
         listView.setAdapter(encountersArrayAdapter);
 
-//        encounterButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//                enterEncounter = (EditText) findViewById(R.id.enterEncounter);
-//                newEncounter = enterEncounter.getText().toString();
-//
-//                encounterList.add (newEncounter);
-//                encountersArrayAdapter.notifyDataSetChanged();
-//            }
-//        });
+//        combatantsArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, combatantList);
+//        combatantList.setAdapter(combatantsArrayAdapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+                Intent intent = new Intent(view.getContext(), Combatant.class);
+                startActivity(intent);
+        }
+        });
     }
 
     public void addEncounter(View v) {
@@ -60,4 +64,10 @@ public class MainActivity extends AppCompatActivity {
             enterEncounter.setText("");
         }
     }
+
+//    public void openEncounter(View v) {
+//        Intent intent = new Intent(this, Combatant.class);
+//        ListView combatants = (ListView) findViewById(R.id.combatantView);
+//        startActivity(intent);
+//    }
 }
