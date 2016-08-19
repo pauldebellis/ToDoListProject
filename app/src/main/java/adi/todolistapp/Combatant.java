9c65e00299249e2b8e288fa3170dbfd7dbbd6eaa
pauldebellis <pvdebellis@gmail.com>
2016-08-18 17:16:43 -0700

    I'm bad at git

--- app/src/main/java/adi/todolistapp/Combatant.java
+++ app/src/main/java/adi/todolistapp/Combatant.java
@@ -3,8 +3,13 @@ package adi.todolistapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -14,14 +19,16 @@ import java.util.ArrayList;
 public class Combatant extends AppCompatActivity {
 
     private String title;
-//    private ArrayList<String> title;
+    //    private ArrayList<String> title;
     private TextView titleView;
     private ArrayList<String> combatantList;
     private ListView listView;
     private EditText enterCombatant;
     private String newCombatant;
-    private ArrayAdapter <String> combatantArrayAdapter;
+    private ArrayAdapter<String> combatantArrayAdapter;
     private ArrayList<ArrayList<String>> encounterArray;
+    private Button combatantButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +48,14 @@ public class Combatant extends AppCompatActivity {
         combatantArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, combatantList);
         listView.setAdapter(combatantArrayAdapter);
 
+        Button combatantButton = (Button)findViewById(R.id.combatantButton);
+        combatantButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addCombatant(enterCombatant);
+            }
+        });
+
 
 
 //        MAYBE MAKE ITEMS STRIKETHROUGH ON CLICK???
@@ -53,8 +68,6 @@ public class Combatant extends AppCompatActivity {
 //        });
 
 
-
-
     }
 
     public void addCombatant(View v) {
@@ -64,11 +77,34 @@ public class Combatant extends AppCompatActivity {
 
         if (enterCombatant.getText().toString() != "") {
             combatantList.add(newCombatant);
-            encounterArray.add(combatantList);
+//            encounterArray.add(combatantList);
             combatantArrayAdapter.notifyDataSetChanged();
             enterCombatant.setText("");
 
         }
     }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        switch (item.getItemId()) {
+            case R.id.delete:
+                combatantList.remove(info.position);
+                combatantArrayAdapter.notifyDataSetChanged();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+
+        }
+
+
+    }
 }
--- app/src/main/java/adi/todolistapp/MainActivity.java
+++ app/src/main/java/adi/todolistapp/MainActivity.java
@@ -3,9 +3,13 @@ package adi.todolistapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
@@ -19,29 +23,41 @@ public class MainActivity extends AppCompatActivity {
     private ListView listView;
     private EditText enterEncounter;
     private String newEncounter;
+    private Button encounterButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button encounterButton = (Button)findViewById(R.id.encounterButton);
+        encounterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addEncounter(enterEncounter);
+            }
+        });
+
         listView = (ListView) findViewById(R.id.encounters);
+
+        registerForContextMenu(listView);
+
 
         encounterList = new ArrayList<>();
         combatantList = new ArrayList<>();
 
 //        encounterList.add("poop");
 
-        encountersArrayAdapter = new ArrayAdapter<> (this, android.R.layout.simple_list_item_1, encounterList);
+        encountersArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, encounterList);
         listView.setAdapter(encountersArrayAdapter);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(view.getContext(), Combatant.class);
                 intent.putExtra("COMBATANT", encounterList.get(position));
                 startActivity(intent);
-        }
+            }
         });
     }
 
@@ -56,4 +72,32 @@ public class MainActivity extends AppCompatActivity {
             enterEncounter.setText("");
         }
     }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+    }
+
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        switch (item.getItemId()) {
+            case R.id.delete:
+                encounterList.remove(info.position);
+                encountersArrayAdapter.notifyDataSetChanged();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+
+        }
+
+
+//        return super.onContextItemSelected(item);
+    }
+
 }
+
